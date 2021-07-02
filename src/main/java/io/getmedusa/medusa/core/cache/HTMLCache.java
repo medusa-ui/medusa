9c93f52bd17aefe27f29de71a901145401f928fa
Kevin Deyne <kevindeyne@gmail.com>
2021-07-02 10:56:52 -0700

    Update HTMLCache.java

--- src/main/java/io/getmedusa/medusa/core/cache/HTMLCache.java
+++ src/main/java/io/getmedusa/medusa/core/cache/HTMLCache.java
@@ -11,9 +11,13 @@ public class HTMLCache {
     }
     private static final Map<String, String> CACHE = new HashMap<>();
 
-    public String getHTMLOrAdd(String filename, String html) {
-        return CACHE.computeIfAbsent(filename, k -> html);
+     public String getHTMLOrAdd(String filename, String html) {
+        return CACHE.computeIfAbsent(filename, k -> removeAllCommentsFromHTML(html));
     }
+
+    private String removeAllCommentsFromHTML(String html) {
+        return html.replaceAll("(?s)<!--.*?-->", "");
+     }
 
     public String getHTML(String filename) {
         return CACHE.get(filename);
