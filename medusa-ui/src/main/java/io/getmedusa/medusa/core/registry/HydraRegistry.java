f6b0ea8c092654141fd1c6fae0f55246bd79ec33
Kevin Deyne <kevindeyne@gmail.com>
2021-10-05 23:28:50 -0700

    Update existing hydra menus when they are emptied out

--- medusa-ui/src/main/java/io/getmedusa/medusa/core/registry/HydraRegistry.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/core/registry/HydraRegistry.java
@@ -4,6 +4,7 @@ import io.getmedusa.medusa.core.injector.DOMChanges;
 import io.getmedusa.medusa.core.websocket.hydra.HydraMenuItem;
 import io.getmedusa.medusa.core.websocket.hydra.meta.HydraStatus;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -20,13 +21,25 @@ public class HydraRegistry {
     }
 
     public static void update(HydraStatus status) {
+        ensureExistingMenuItemsGetAnEmptyUpdateIfGone(status);
         HydraRegistry.status = status;
+
 
         DOMChanges menuUpdates = DOMChanges.empty();
         for(Map.Entry<String, Set<HydraMenuItem>> menuItemEntry : status.getMenuItems().entrySet()) {
             menuUpdates.and(menuItemEntry.getKey(), menuItemEntry.getValue(), DOMChangeType.HYDRA_MENU);
         }
+        //TODO need an option for 'completely empty', because right now a [] gets returned which gets ignored since it contains no type
 
         ActiveSessionRegistry.getInstance().sendToAll(menuUpdates.build());
+    }
+
+    private static void ensureExistingMenuItemsGetAnEmptyUpdateIfGone(HydraStatus status) {
+        if(HydraRegistry.status == null) return;
+        Set<String> menusThatNeedAnUpdate = new HashSet<>(HydraRegistry.status.getMenuItems().keySet());
+        menusThatNeedAnUpdate.removeAll(status.getMenuItems().keySet());
+        for(String menuThatNeedsAnUpdate : menusThatNeedAnUpdate) {
+            status.getMenuItems().put(menuThatNeedsAnUpdate, new HashSet<>());
+        }
     }
 }
