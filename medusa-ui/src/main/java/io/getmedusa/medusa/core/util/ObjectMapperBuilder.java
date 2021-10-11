b57d64396f50b041b30612009a0deaeaea342c90
Kevin Deyne <kevindeyne@gmail.com>
2021-10-10 22:28:41 -0700

    Update ObjectMapperBuilder.java

--- medusa-ui/src/main/java/io/getmedusa/medusa/core/util/ObjectMapperBuilder.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/core/util/ObjectMapperBuilder.java
@@ -2,6 +2,7 @@ package io.getmedusa.medusa.core.util;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
 public class ObjectMapperBuilder {
     /**
@@ -11,6 +12,7 @@ public class ObjectMapperBuilder {
     public static ObjectMapper setupObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         return objectMapper;
     }
 
