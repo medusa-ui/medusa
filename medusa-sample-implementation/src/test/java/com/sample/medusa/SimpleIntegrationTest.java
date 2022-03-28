67af38f634a579575966ebda6d7e7aee5351a92c
Dirk <dirkdeyne@gmail.com>
2022-03-28 21:32:57 +0200

    fix #174: add missing additional dependencies regarding selenium version update
    - set headless via properties, with false as default (useful for local testing)
    - provide a way to open url in new tab or window (the selenium 4 way)
    - provide a way to switch between windows

--- medusa-sample-implementation/src/test/java/com/sample/medusa/SimpleIntegrationTest.java
+++ medusa-sample-implementation/src/test/java/com/sample/medusa/SimpleIntegrationTest.java
@@ -2,10 +2,11 @@ package com.sample.medusa;
 
 import com.sample.medusa.meta.AbstractSeleniumTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT /*, properties = "headless:false" */ )
 class SimpleIntegrationTest extends AbstractSeleniumTest {
 
     @Test
@@ -15,4 +16,37 @@ class SimpleIntegrationTest extends AbstractSeleniumTest {
         Assertions.assertTrue(driver.getPageSource().contains("Hello Medusa"));
     }
 
+    @Test
+    @DisplayName("open a second tab")
+    void testOpenTab() throws Exception{
+        String original = goTo("/");
+        Assertions.assertTrue(driver.getPageSource().contains("Hello Medusa"));
+
+        String window = openInNewTab("/test/click-event");
+        Assertions.assertTrue(driver.getPageSource().contains("Hello click event"));
+
+        switchToWindowOrTab(original);
+        Assertions.assertTrue(driver.getPageSource().contains("Hello Medusa"));
+
+        switchToWindowOrTab(window);
+        Assertions.assertTrue(driver.getPageSource().contains("Hello click event"));
+
+    }
+
+    @Test
+    @DisplayName("open a second window")
+    void TestOpenWindow() throws Exception{
+        String original = goTo("/");
+        Assertions.assertTrue(driver.getPageSource().contains("Hello Medusa"));
+
+        String window = openInNewWindow("/test/click-event");
+        Assertions.assertTrue(driver.getPageSource().contains("Hello click event"));
+
+        switchToWindowOrTab(original);
+        Assertions.assertTrue(driver.getPageSource().contains("Hello Medusa"));
+
+        switchToWindowOrTab(window);
+        Assertions.assertTrue(driver.getPageSource().contains("Hello click event"));
+
+    }
 }
