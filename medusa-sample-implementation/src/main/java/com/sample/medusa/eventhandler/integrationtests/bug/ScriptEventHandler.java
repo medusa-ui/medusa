c9fe4eeba20bf3cec5157146b0f6cdc75d866f91
Dirk <dirkdeyne@gmail.com>
2022-03-30 08:20:01 +0200

    fix #170: The content of script-tags should not get encoded (#177)
    done by surrounding the content with '/* <!--CDATA[[ */' and '/* ]]--> */'

---
+++ medusa-sample-implementation/src/main/java/com/sample/medusa/eventhandler/integrationtests/bug/ScriptEventHandler.java
@@ -0,0 +1,7 @@
+package com.sample.medusa.eventhandler.integrationtests.bug;
+
+import io.getmedusa.medusa.core.annotation.UIEventPage;
+
+@UIEventPage(path="/test/bug/script", file = "pages/integration-tests/bug/script.html")
+public class ScriptEventHandler {
+}
