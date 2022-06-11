29bfb8121ec2f4aaf33e4704f6566e1f0a3b6ed0
Kevin Deyne <kevindeyne@gmail.com>
2022-06-10 20:32:04 -0700

    Better channel impl

--- medusa-ui/src/main/resources/static/index.js
+++ medusa-ui/src/main/resources/static/index.js
@@ -1,5 +1,5 @@
-const {RSocketConnector} = require("rsocket-core");
-const {WebsocketClientTransport} = require("rsocket-websocket-client");
+const { RSocketConnector } = require("rsocket-core");
+const { WebsocketClientTransport } = require("rsocket-websocket-client");
 const {
     encodeCompositeMetadata,
     encodeRoute,
@@ -7,8 +7,7 @@ const {
     encodeSimpleAuthMetadata,
 } = require("rsocket-composite-metadata");
 
-let client = undefined;
-let rsocket = undefined;
+const MAX_REQUEST_N = 2147483647;
 
 function addErrorMessage(prefix, error) {
     var ul = document.getElementById("messages");
@@ -25,66 +24,78 @@ function addMessage(message) {
     ul.appendChild(li);
 }
 
-async function main() {
-    if (rsocket !== undefined) {
-        //rsocket.close();
-        document.getElementById("messages").innerHTML = "";
-    }
+//TODO https://github.com/viglucci/rsocket-chat-demo/blob/main/frontend/src/context/ClientProvider.jsx
+//TODO read https://github.com/viglucci/rsocket-chat-demo/blob/main/frontend/src/hooks/useChat.js
 
-    // Create an instance of a client
-    client = new RSocketConnector({
+async function setupRouter() {
+    const map = new Map();
+    map.set(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION, encodeSimpleAuthMetadata("user", "pass"));
+    const compositeMetaData = encodeCompositeMetadata(map);
+
+    const connector = new RSocketConnector({
         setup: {
-            // ms btw sending keepalive to server
+            payload: {
+                metadata: compositeMetaData
+            },
             keepAlive: 60000,
-            // ms timeout if no keepalive response
             lifetime: 180000,
-            // format of `data`
             dataMimeType: WellKnownMimeType.APPLICATION_JSON.string,
-            // format of `metadata`
             metadataMimeType: WellKnownMimeType.MESSAGE_RSOCKET_COMPOSITE_METADATA.string,
         },
         transport: new WebsocketClientTransport({
             url: 'ws://localhost:7000/tweetsocket'
         }),
     });
+    const rsocket = await connector.connect();
+    stream = await buildStream(rsocket);
+}
 
-    rsocket = await client.connect();
-    await new Promise((resolve, reject) => {
-        const encodedRoute = encodeRoute('tweets.by.author');
+let stream;
+async function buildStream(rsocket) {
+    const encodedRoute = encodeRoute('tweets.by.author');
+    const map = new Map();
+    map.set(WellKnownMimeType.MESSAGE_RSOCKET_ROUTING, encodedRoute);
+    map.set(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION, encodeSimpleAuthMetadata("user", "pass"));
+    const compositeMetaData = encodeCompositeMetadata(map);
 
-        const map = new Map();
-        map.set(WellKnownMimeType.MESSAGE_RSOCKET_ROUTING, encodedRoute);
-        map.set(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION, encodeSimpleAuthMetadata("user", "pass"));
-        const compositeMetaData = encodeCompositeMetadata(map);
-
-        let payloadData = {author: document.getElementById("author-filter").value};
-
-        const requester = rsocket.requestChannel(
-            {
-                data: Buffer.from(JSON.stringify(payloadData)),
-                metadata: compositeMetaData,
-            },
-            1,
-            false,
-            {
-                onError: error => {
-                    console.error(error);
-                    addErrorMessage("Connection has been closed due to ", error);
-                },
-                onNext: payload => {
-                    console.log("onNext", payload.data.toString());
-                    addMessage(JSON.parse(payload.data.toString()));
-                },
-                onSubscribe: subscription => {
-                    console.log("onSubscribe", subscription);
-                },
-                cancel: () => { },
-                request: () => { },
-                onComplete: () => console.log('Request-channel completed')
-            }
-        );
+    return rsocket.requestChannel({
+        metadata: compositeMetaData
+    },
+    MAX_REQUEST_N,
+    false,
+    {
+        onError(error) {
+            console.error(error);
+        },
+        onComplete() {
+            console.error('peer stream complete');
+        },
+        onNext(payload, isComplete) {
+            console.log(
+                `payload[data: ${payload.data}; metadata: ${payload.metadata}]|${isComplete}`
+            );
+        },
+        onExtension(extendedType, content, canBeIgnored) {
+        },
+        request(requestN) {
+            console.log(`peer requested ${requestN}`)
+        },
+        cancel() {
+            console.log(`peer canceled`)
+        }
     });
 }
 
-document.addEventListener('DOMContentLoaded', main);
-document.getElementById('author-filter').addEventListener('change', main);
+function sendNextMessage() {
+    let payloadData = { author: document.getElementById("author-filter").value };
+    sendMessage(payloadData);
+}
+
+function sendMessage(payloadData) {
+    stream.onNext({
+        data: Buffer.from(JSON.stringify(payloadData))
+    });
+}
+
+document.addEventListener('DOMContentLoaded', setupRouter);
+document.getElementById('author-filter').addEventListener('change', sendNextMessage);
