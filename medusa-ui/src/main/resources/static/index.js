6fc327f4ba9785480f41a717b24246079d19d80b
Kevin Deyne <kevindeyne@gmail.com>
2022-07-21 23:53:27 -0700

    XSS protection method rename

--- medusa-ui/src/main/resources/static/index.js
+++ medusa-ui/src/main/resources/static/index.js
@@ -1,6 +1,6 @@
 const { RSocketConnector } = require("rsocket-core");
 const { WebsocketClientTransport } = require("rsocket-websocket-client");
-const sanitizeUrl = require("@braintree/sanitize-url").sanitizeUrl;
+const escape = require("@braintree/sanitize-url").sanitizeUrl;
 const {
     encodeCompositeMetadata,
     encodeRoute,
@@ -138,7 +138,7 @@ applyAllChanges = function (listOfDiffs) {
         } else if(diff.type === "REMOVAL") {
             handleRemoval(diff);
         } else if(diff.type === "REDIRECT") {
-            window.location.href = sanitizeUrl(diff.content);
+            window.location.href = escape(diff.content);
         }
     }
 };
