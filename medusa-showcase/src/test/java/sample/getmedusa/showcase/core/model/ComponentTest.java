e71678f62b345b8b8a421dd5b61c521dea647ed0
Kevin Deyne <kevindeyne@gmail.com>
2022-08-02 23:50:16 -0700

    Update ComponentTest.java

--- medusa-showcase/src/test/java/sample/getmedusa/showcase/core/model/ComponentTest.java
+++ medusa-showcase/src/test/java/sample/getmedusa/showcase/core/model/ComponentTest.java
@@ -7,13 +7,19 @@ class ComponentTest {
 
     @Test
     void testURIPartCreation() {
-        Component c = new Component("Multiple selection list");
+        Component c = new Component("Multiple selection list", "multiple-selection-list");
         Assertions.assertEquals("Multiple selection list", c.getLabel());
         Assertions.assertEquals("multiple-selection-list", c.getUrlPart());
+        Assertions.assertFalse(c.isComingSoon());
 
-        c = new Component("Modal / Alert");
+        c = new Component("Modal / Alert", "modal-alert");
         Assertions.assertEquals("Modal / Alert", c.getLabel());
         Assertions.assertEquals("modal-alert", c.getUrlPart());
+        Assertions.assertFalse(c.isComingSoon());
+
+        c = new Component("Something else");
+        Assertions.assertEquals("Something else", c.getLabel());
+        Assertions.assertTrue(c.isComingSoon());
     }
 
 }
