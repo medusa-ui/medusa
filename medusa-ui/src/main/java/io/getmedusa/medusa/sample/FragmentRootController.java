2c150da5be6f4c97ebb9a268ababb3df438a13a2
Kevin Deyne <kevindeyne@gmail.com>
2022-08-05 22:02:32 -0700

    Update FragmentRootController.java

--- medusa-ui/src/main/java/io/getmedusa/medusa/sample/FragmentRootController.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/sample/FragmentRootController.java
@@ -4,16 +4,13 @@ import io.getmedusa.medusa.core.annotation.UIEventPage;
 import io.getmedusa.medusa.core.attributes.Attribute;
 import org.springframework.web.reactive.function.server.ServerRequest;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @UIEventPage(path = "/fragment", file = "/pages/fragment-root")
 public class FragmentRootController {
 
     public List<Attribute> setupAttributes(ServerRequest serverRequest) {
-        List<Person> people = new ArrayList<>();//generateListOfPeople();
         return List.of(
-                //new Attribute("counterValue", Integer.parseInt(serverRequest.pathVariable("samplePathVariable"))),
                 new Attribute("service", "self"),
                 new Attribute("ref", "search-bar")
         );
