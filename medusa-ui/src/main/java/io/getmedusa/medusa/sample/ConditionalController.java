970291b06bd255f5c4647e19f046da91cc3fac45
Dirk <dirkdeyne@gmail.com>
2022-08-19 20:12:18 +0200

    bug conditional
    try to simplify the problem...

--- medusa-ui/src/main/java/io/getmedusa/medusa/sample/ConditionalController.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/sample/ConditionalController.java
@@ -12,12 +12,17 @@ import java.util.ListIterator;
 public class ConditionalController {
 
     private Integer number = 42;
-    private List<Integer> fibonacci = List.of( 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765);
+    private Boolean show = Boolean.TRUE;
+    // Yes, I know the Fibonacci sequence contains two ones... but the second 1 is not useful here
+    private List<Integer> fibonacci = List.of( 0, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765);
 
     public List<Attribute> setupAttributes(){
         return List.of(
-           new Attribute("number", number),
-           new Attribute("fibo", fibonacciSum(number))
+               new Attribute("number", number),
+               new Attribute("fibo", fibonacciSum(number)),
+
+               new Attribute("show", true),
+               new Attribute("message", "Now, You see me")
         );
     }
 
@@ -28,6 +33,17 @@ public class ConditionalController {
                 new Attribute("fibo", result)
         );
     }
+    // try to simplify the problem
+    public List<Attribute> toggleShow() {
+        show = !show;
+        String message =  show ? "Now, You see me" : "Now, You don't";
+        System.out.println("show: " + show + ", message: " + message);
+        return List.of(
+                new Attribute("message", message),
+                new Attribute("show", show)
+        );
+    }
+
 
     private List<Integer> fibonacciSum(Integer number) {
         List<Integer> result = new ArrayList<>();
