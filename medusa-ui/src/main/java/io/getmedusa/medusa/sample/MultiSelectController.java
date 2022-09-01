6a361602fe0845c023abc5902cc2e76510359889
Dirk <dirkdeyne@gmail.com>
2022-09-01 20:37:50 +0200

    initial attempt to use multi select
    with map: only one (last) option
    with formObject: error

---
+++ medusa-ui/src/main/java/io/getmedusa/medusa/sample/MultiSelectController.java
@@ -0,0 +1,41 @@
+package io.getmedusa.medusa.sample;
+
+import io.getmedusa.medusa.core.annotation.UIEventPage;
+import io.getmedusa.medusa.core.attributes.Attribute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
+
+@UIEventPage(path = "/multi-select", file = "/pages/multi-select")
+public class MultiSelectController {
+
+    private static final Logger logger = LoggerFactory.getLogger(MultiSelectController.class);
+
+    public List<Attribute> setupAttributes() {
+        return List.of(
+                new Attribute("favoriteFruits", List.of("Orange","Banana")),
+                new Attribute("fruits", List.of("Apple", "Banana", "Lemon", "Orange", "Strawberry"))
+        );
+    }
+
+    /* use map */
+    public List<Attribute> favoritesMap(Map<String, Object> form) {
+        logger.info("Map: " + form);
+        return List.of(
+                new Attribute("favoriteFruits", List.of("Banana"))
+        );
+    }
+
+    /* use FormObject */
+    public List<Attribute> favoritesFormObject(FormObject form) {
+        logger.info("FormObject: " + form);
+        return List.of(
+                new Attribute("favoriteFruits", List.of("Banana"))
+        );
+    }
+
+    public record FormObject(List<String> favoriteFruits){}
+
+}
