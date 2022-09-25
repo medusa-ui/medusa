2328e82f6b9f9795c402046ea0720bbd8a97e7c6
Kevin Deyne <kevindeyne@gmail.com>
2022-09-24 23:06:21 -0700

    #314 Loading fixes

--- medusa-showcase/src/main/java/sample/getmedusa/showcase/samples/loading/LoadingController.java
+++ medusa-showcase/src/main/java/sample/getmedusa/showcase/samples/loading/LoadingController.java
@@ -3,19 +3,32 @@ package sample.getmedusa.showcase.samples.loading;
 import io.getmedusa.medusa.core.annotation.UIEventPage;
 import io.getmedusa.medusa.core.attributes.Attribute;
 import io.getmedusa.medusa.core.attributes.StandardAttributeKeys;
+import io.getmedusa.medusa.core.bidirectional.ServerToClient;
+import io.getmedusa.medusa.core.session.StandardSessionTagKeys;
 
 import java.util.List;
 
 @UIEventPage(path = "/detail/sample/loading", file = "/pages/sample/loading.html")
 public class LoadingController {
 
-    public List<Attribute> loadingLogic() {
-        try {
-            Thread.sleep(2000);
-        } catch (Exception e) {
-            //ignore
-        }
-        return List.of(new Attribute(StandardAttributeKeys.LOADING, "loading-done"));
+    private final ServerToClient serverToClient;
+
+    public LoadingController(ServerToClient serverToClient) {
+        this.serverToClient = serverToClient;
+    }
+
+    public void loadingLogic() {
+        new Thread(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+                //ignore
+            }
+            serverToClient.sendAttributesToSessionTag(
+                    List.of(new Attribute(StandardAttributeKeys.LOADING, "loading-done")),
+                    StandardSessionTagKeys.ROUTE,
+                    "/detail/loading");
+        }).start();
     }
 
 }
