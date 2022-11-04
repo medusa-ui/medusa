205afb82005597074fbd5ccd57314997a3d25bc5
Kevin Deyne <kevindeyne@gmail.com>
2022-11-04 14:57:31 -0700

    Added child sequence re-ordering as a new event

--- medusa-ui/src/main/java/io/getmedusa/medusa/core/diffengine/DiffComparator.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/core/diffengine/DiffComparator.java
@@ -34,6 +34,15 @@ public class DiffComparator implements Comparator<JSReadyDiff> {
             o1Value = 0;
         }
 
+        //do removals last
+        if(o1.isRemoval() && !o2.isRemoval()) {
+            o2Value = 0;
+            o1Value = 1;
+        } else if(o2.isRemoval() && !o1.isRemoval()) {
+            o2Value = 1;
+            o1Value = 0;
+        }
+
         return o1Value - o2Value;
     }
 
