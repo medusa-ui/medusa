628d60651245a20520b904b7d07408adab328fe3
Kevin Deyne <kevindeyne@gmail.com>
2023-01-09 19:07:34 -0800

    Updated example with alternative way to add attributes

--- medusa-showcase/src/main/java/sample/getmedusa/showcase/samples/button/basic/BasicButtonController.java
+++ medusa-showcase/src/main/java/sample/getmedusa/showcase/samples/button/basic/BasicButtonController.java
@@ -6,19 +6,22 @@ import io.getmedusa.medusa.core.session.Session;
 
 import java.util.List;
 
+import static io.getmedusa.medusa.core.attributes.Attribute.$;
+import static io.getmedusa.medusa.core.attributes.Attribute.$$;
+
 @UIEventPage(path = "/detail/sample/basic-button", file = "/pages/sample/basic-button.html")
 public class BasicButtonController {
 
     protected static final String COUNTER = "counter";
 
     public List<Attribute> setupAttributes() {
-        return List.of(new Attribute(COUNTER, 0));
+        return $$(COUNTER, 0);
     }
 
     public List<Attribute> updateCounter(int amount, Session session) {
         int counter = session.getAttribute(COUNTER);
         counter += amount;
-        return List.of(new Attribute(COUNTER, counter));
+        return $$($(COUNTER, counter));
     }
 
     public List<Attribute> reset() {
