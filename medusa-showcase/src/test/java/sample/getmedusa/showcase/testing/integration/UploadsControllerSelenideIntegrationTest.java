d10c87e418003ff2cdb3c48b649756898c190386
Dirk <dirkdeyne@gmail.com>
2023-03-17 19:56:20 +0100

    upload a single file (image)
    integration test

---
+++ medusa-showcase/src/test/java/sample/getmedusa/showcase/testing/integration/UploadsControllerSelenideIntegrationTest.java
@@ -0,0 +1,38 @@
+package sample.getmedusa.showcase.testing.integration;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sample.getmedusa.showcase.testing.integration.meta.SelenideIntegrationTest;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
+import static org.openqa.selenium.By.id;
+
+public class UploadsControllerSelenideIntegrationTest extends SelenideIntegrationTest {
+
+    @BeforeEach
+    void setup(){
+        openPage("uploads");
+    }
+
+    @Test
+    void uploadFile() {
+        // initial
+        $(id("img_upload")).shouldNotBe(Condition.visible);
+        $(id("prg_upload")).shouldNot(Condition.appear);
+
+        // when
+        $(id("single_file")).uploadFromClasspath("tree.png");
+        $(id("btn_upload")).click();
+
+        // then
+        $(id("prg_upload")).shouldBe(appear);
+
+        // and
+        $(id("prg_upload")).should(disappear);
+
+        // and
+        $(id("img_upload")).shouldBe(visible);
+    }
+}
