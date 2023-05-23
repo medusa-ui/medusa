0ed67c22e335106436ce9f6a0774a12b6a066ec5
Kevin Deyne <kevindeyne@gmail.com>
2023-05-22 20:56:55 -0700

    Update DetailController.java

--- medusa-showcase/src/main/java/sample/getmedusa/showcase/core/controller/DetailController.java
+++ medusa-showcase/src/main/java/sample/getmedusa/showcase/core/controller/DetailController.java
@@ -2,23 +2,14 @@ package sample.getmedusa.showcase.core.controller;
 
 import io.getmedusa.medusa.core.annotation.UIEventPage;
 import io.getmedusa.medusa.core.attributes.Attribute;
-import org.springframework.core.io.DefaultResourceLoader;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import sample.getmedusa.showcase.core.Versions;
 import sample.getmedusa.showcase.core.model.Component;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 @UIEventPage(path = "/detail/{type}", file = "/pages/detail")
-public class DetailController {
+public class DetailController extends AbstractController {
 
     public List<Attribute> setupAttributes(ServerRequest request) {
         final Component component = Component.findComponent(request.pathVariable("type"));
@@ -28,21 +19,6 @@ public class DetailController {
                 new Attribute("serverCode", loadCode(component.getServerCode())),
                 new Attribute("clientCode", loadCode(component.getClientCode())),
                 new Attribute("version", Versions.getVersionFooter()));
-    }
-
-    private final ResourceLoader resourceLoader = new DefaultResourceLoader();
-
-    public String[] loadCode(String ... paths) {
-        List<String> code = new ArrayList<>();
-        for(String path : paths) {
-            Resource resource = resourceLoader.getResource("classpath:" + path);
-            try (Reader reader = new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8)) {
-                code.add(FileCopyUtils.copyToString(reader));
-            } catch (IOException e) {
-                code.add("No code found");
-            }
-        }
-        return code.toArray(new String[0]);
     }
 
 }
