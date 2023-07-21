73648ed6ca066ef82b7b93905e71f56c7ed66eb2
Kevin Deyne <kevindeyne@gmail.com>
2023-07-20 20:03:22 -0700

    Added missing import

--- medusa-ui/src/main/java/io/getmedusa/medusa/core/config/RSocketSecurityConfiguration.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/core/config/RSocketSecurityConfiguration.java
@@ -1,10 +1,11 @@
 package io.getmedusa.medusa.core.config;
 
+import io.getmedusa.medusa.core.memory.SessionUserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 import org.springframework.security.config.annotation.rsocket.EnableRSocketSecurity;
 import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
 import org.springframework.security.messaging.handler.invocation.reactive.AuthenticationPrincipalArgumentResolver;
