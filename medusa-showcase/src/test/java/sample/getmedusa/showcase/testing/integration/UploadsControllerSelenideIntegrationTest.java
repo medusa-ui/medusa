fd847ed944fd3dafba6207dc167fffde24bccf87
Dirk <dirkdeyne@gmail.com>
2023-07-31 16:58:05 +0200

    upload image: test validation

--- medusa-showcase/src/test/java/sample/getmedusa/showcase/testing/integration/UploadsControllerSelenideIntegrationTest.java
+++ medusa-showcase/src/test/java/sample/getmedusa/showcase/testing/integration/UploadsControllerSelenideIntegrationTest.java
@@ -47,4 +47,26 @@ class UploadsControllerSelenideIntegrationTest extends SelenideIntegrationTest {
         // then
         $(id("img_upload")).shouldBe(visible);
     }
+
+    @Test
+    void uploadValidation() {
+        // initial
+        $(id("img_upload")).shouldNotBe(Condition.visible);
+        $(id("prg_upload")).shouldNot(Condition.appear);
+
+        // when file is too big
+        $(id("single_file")).uploadFromClasspath("tree_3.png");
+        $(id("btn_upload")).click();
+
+        // then
+        $(id("validation_msg")).shouldBe(visible);
+        $(id("validation_msg")).shouldBe(exactText("File size exceeds the maximum allowed size"));
+
+        // when file size is ok
+        $(id("single_file")).uploadFromClasspath("tree.png");
+        $(id("btn_upload")).click();
+
+        // then
+        $(id("prg_upload")).shouldBe(appear);
+    }
 }
