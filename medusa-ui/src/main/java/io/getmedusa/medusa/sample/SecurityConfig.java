d7d50afa50f618c42049e88632bdf5bde8a31eac
Dirk <dirkdeyne@gmail.com>
2023-09-11 11:29:34 +0200

    reworked deprecated spring security method calls

--- medusa-ui/src/main/java/io/getmedusa/medusa/sample/SecurityConfig.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/sample/SecurityConfig.java
@@ -2,9 +2,11 @@ package io.getmedusa.medusa.sample;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 
 @Configuration
@@ -12,11 +14,20 @@ import org.springframework.security.web.server.util.matcher.ServerWebExchangeMat
 public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http.authorizeExchange()
-                .anyExchange().permitAll()
-                .and().csrf().requireCsrfProtectionMatcher(
-                        serverWebExchange -> ServerWebExchangeMatchers.pathMatchers("/urls-with-csrf-check/**").matches(serverWebExchange)
-                )
-                .and().build();
+        return http.authorizeExchange(authorizeExchangeCustomizer())
+                   .csrf(csrfCustomizer())
+                   .build();
+    }
+
+    private static Customizer<ServerHttpSecurity.AuthorizeExchangeSpec> authorizeExchangeCustomizer() {
+        return authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().permitAll();
+    }
+
+    private static Customizer<ServerHttpSecurity.CsrfSpec> csrfCustomizer() {
+        return csrfSpec -> csrfSpec.requireCsrfProtectionMatcher(csrfRequiredMatcher());
+    }
+
+    private static ServerWebExchangeMatcher csrfRequiredMatcher() {
+        return exchange -> ServerWebExchangeMatchers.pathMatchers("/urls-with-csrf-check/**").matches(exchange);
     }
 }
