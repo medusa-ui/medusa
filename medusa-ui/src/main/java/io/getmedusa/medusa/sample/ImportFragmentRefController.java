a3c977459aa28829e8f114494cd5fda5534dd6b6
Kevin Deyne <kevindeyne@gmail.com>
2023-12-10 17:24:15 -0800

    Add failing actions, add imports for showcase

--- medusa-ui/src/main/java/io/getmedusa/medusa/sample/ImportFragmentRefController.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/sample/ImportFragmentRefController.java
@@ -1,11 +1,25 @@
 package io.getmedusa.medusa.sample;
 
 import io.getmedusa.medusa.core.annotation.UIEventPage;
+import io.getmedusa.medusa.core.attributes.Attribute;
+import io.getmedusa.medusa.core.session.Session;
+
+import java.util.List;
+
+import static io.getmedusa.medusa.core.attributes.Attribute.$$;
 
 //ref fragment which actually imports the variable
 @UIEventPage(path = "/fragments/import-ref", file = "/pages/fragments/import-fragment-ref")
 public class ImportFragmentRefController {
 
+    public List<Attribute> setupAttributes() {
+        return $$("counter", 0);
+    }
 
+    public List<Attribute> updateCounter(int amount, Session session) {
+        int counter = session.getAttribute("counter");
+        counter += amount;
+        return $$("counter", counter);
+    }
 
 }
--- medusa-ui/src/main/java/io/getmedusa/medusa/sample/ImportFragmentRootController.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/sample/ImportFragmentRootController.java
@@ -5,20 +5,19 @@ package io.getmedusa.medusa.sample;
 
 import io.getmedusa.medusa.core.annotation.UIEventPage;
 import io.getmedusa.medusa.core.attributes.Attribute;
-import org.springframework.web.reactive.function.server.ServerRequest;
 
 import java.util.List;
 import java.util.Random;
+
+import static io.getmedusa.medusa.core.attributes.Attribute.$$;
 
 @UIEventPage(path = "/import-fragment", file = "/pages/import-fragment")
 public class ImportFragmentRootController {
 
     private static final Random RANDOM = new Random();
 
-    public List<Attribute> setupAttributes(ServerRequest serverRequest) {
-        return List.of(
-                new Attribute("rootValue", RANDOM.nextInt(999))
-        );
+    public List<Attribute> setupAttributes() {
+        return $$("rootValue", RANDOM.nextInt(999));
     }
 
 }
