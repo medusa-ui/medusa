be2bee330a7981080438939af3c172694e1e9d05
Dirk <dirkdeyne@gmail.com>
2024-01-30 15:43:08 +0100

    Medusa Tag processing abstraction

--- medusa-ui/src/main/java/io/getmedusa/medusa/core/tags/meta/MedusaElementTagProcessor.java
+++ medusa-ui/src/main/java/io/getmedusa/medusa/core/tags/meta/MedusaElementTagProcessor.java
@@ -1,13 +1,19 @@
 package io.getmedusa.medusa.core.tags.meta;
 
-import org.thymeleaf.processor.element.AbstractElementTagProcessor;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.thymeleaf.model.IModel;
+import org.thymeleaf.processor.element.AbstractElementModelProcessor;
+
+import java.util.List;
 
 import static io.getmedusa.medusa.core.tags.annotation.MedusaTag.*;
 
 /**
  * Medusa Tag Processor
  */
-public abstract class MedusaElementTagProcessor extends AbstractElementTagProcessor {
+public abstract class MedusaElementTagProcessor extends AbstractElementModelProcessor {
 
     public static final int PRECEDENCE = 1000;
 
@@ -25,5 +31,16 @@ public abstract class MedusaElementTagProcessor extends AbstractElementTagProces
                 null,
                 false,
                 PRECEDENCE);
+    }
+
+    /**
+     * Retrieve the child Elements form the given model
+     *
+     * @param model the model
+     * @return list of Elements
+     */
+    protected List<Element>  getNodeList(IModel model) {
+        Document document = Jsoup.parse(model.toString());
+        return document.body().getAllElements();
     }
 }
