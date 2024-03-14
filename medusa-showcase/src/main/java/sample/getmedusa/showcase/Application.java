9aa4fecaa30a572b5f9aae00440a5eabc2e864dd
Dirk <dirkdeyne@gmail.com>
2024-03-14 05:19:37 +0100

    Versions in footer (#647)
    * Versions jdk and medusa can be retrieved dynamically
    
    * Versions jdk and medusa can be retrieved dynamically
    
    retrieve versions via maven properties and provide default values

--- medusa-showcase/src/main/java/sample/getmedusa/showcase/Application.java
+++ medusa-showcase/src/main/java/sample/getmedusa/showcase/Application.java
@@ -1,13 +1,26 @@
 package sample.getmedusa.showcase;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import sample.getmedusa.showcase.core.Versions;
 
 @SpringBootApplication
 public class Application {
 
+	@Value("${java.version:21}") String javaVersion;
+	@Value("${medusa.version:0.9.5}") String medusaVersion;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@EventListener(ApplicationReadyEvent.class)
+	void setVersions() {
+		Versions.V_JDK = javaVersion;
+		Versions.V_MEDUSA_UI = medusaVersion;
 	}
 
 }
--- medusa-showcase/src/main/java/sample/getmedusa/showcase/core/Versions.java
+++ medusa-showcase/src/main/java/sample/getmedusa/showcase/core/Versions.java
@@ -4,11 +4,11 @@ public class Versions {
 
     private Versions() {}
 
-    private static final String V_MEDUSA_UI = "0.9.5";
-    private static final String V_JDK = "20";
+    public static String V_MEDUSA_UI = "0.9.5";
+    public static String V_JDK = "20";
 
     public static String getVersionFooter() {
-        return String.format("JDK%s w/ Medusa %s", V_JDK, V_MEDUSA_UI);
+        return java.lang.String.format("JDK%s w/ Medusa %s", V_JDK, V_MEDUSA_UI);
     }
 
 }
